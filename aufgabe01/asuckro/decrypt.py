a8fc6ac129d22b824e1bd6b679df9f2459ba0c26
AndreaSuckro <a.suckro@gmail.com>
2016-01-14 18:53:02 +0100

    noch mehr zeug in python getestet

--- aufgabe01/asuckro/decrypt.py
+++ aufgabe01/asuckro/decrypt.py
@@ -5,5 +5,4 @@ key = list('chaostreffosnabrueck')
 text = list(str(sys.argv[1])) #hope it is the first and only argument :D
 
 #decode each character
-for i in range(0,len(text)):
-    print chr((ord(text[i]) - 97 - ord(key[i%len(key)])-97)%26 + 97),
+print "".join([chr((ord(text[i]) - 97 - ord(key[i%len(key)])+97)%26 + 97) for i in range(0,len(text))])
--- aufgabe01/asuckro/encrypt.py
+++ aufgabe01/asuckro/encrypt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+from __future__ import print_function
 import sys
 
 key = list('chaostreffosnabrueck')
@@ -6,4 +7,4 @@ text = list(str(sys.argv[1])) #hope it is the first and only argument :D
 
 #encode each character
 for i in range(0,len(text)):
-    print chr((ord(text[i]) - 97 + ord(key[i%len(key)])-97)%26 + 97),
+    print (chr((ord(text[i]) - 97 + ord(key[i%len(key)])-97)%26 + 97),end='')
