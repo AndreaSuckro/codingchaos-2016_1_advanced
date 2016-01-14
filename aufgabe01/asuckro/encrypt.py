5e05bba8f8477270e974d3884ff448357fd0456a
AndreaSuckro <a.suckro@gmail.com>
2016-01-14 18:46:38 +0100

    schoenere schleifen

--- aufgabe01/asuckro/encrypt.py
+++ aufgabe01/asuckro/encrypt.py
@@ -5,7 +5,5 @@ key = list('chaostreffosnabrueck')
 text = list(str(sys.argv[1])) #hope it is the first and only argument :D
 
 #encode each character
-i = 0
-for c in text:
-    print chr((ord(c) - 97 + ord(key[i%len(key)])-97)%26 + 97),
-    i = i + 1
+for i in range(0,len(text)):
+    print chr((ord(text[i]) - 97 + ord(key[i%len(key)])-97)%26 + 97),
